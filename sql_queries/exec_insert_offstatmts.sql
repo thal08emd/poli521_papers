ec0c395083ec3c3554525fb732298dd621baf463
thal08emd <90644807+thal08emd@users.noreply.github.com>
2022-03-31 17:01:10 -0400

    Update exec_insert_offstatmts.sql

--- sql_queries/exec_insert_offstatmts.sql
+++ sql_queries/exec_insert_offstatmts.sql
@@ -6,24 +6,24 @@ EXEC dbo.InsertingForeignPolicyStatements
                 @init_entity = 'EU' ,
                 @idate = '2008-08-13' ,
                 @iyear = '2008' ,
-                @crit1 = '0' ,
-                @crit2 = '0' ,
-                @crit3 = '1' ,
+                @crit1 = 0 ,
+                @crit2 = 0 ,
+                @crit3 = 1 ,
                 @sanctions = NULL ,
                 @targsanctions = NULL ,
                 @bigfour = NULL ,
                 @EUpresidency = 'France'  ,
-                @tactical = '0' ,
-                @rhetorical = '1' ,
-                @threat_scope = '0' , /*Europe - 0; EU = 1*/
-                @membership_lang = '0' , /*inclusive - 0; exclusive = 1*/
-                @verb_tense = '1' , /*past - 0; present = 1*/
-                @active_passive = '1' /*active - 0; passive = 1*/,
-                @human_rights = '1' ,
-                @ter_sovereignty = '1' ,
-                @peace = '1' ,
-                @securit = '1' , 
-                @democracy = '0' ,
-                @soc_dev = '0' ,
-                @econ_dec = '0' ,
+                @tactical = 0 ,
+                @rhetorical = 1 ,
+                @threat_scope = 0 , /*Europe - 0; EU = 1*/
+                @membership_lang = 0 , /*inclusive - 0; exclusive = 1*/
+                @verb_tense = 1, /*past - 0; present = 1*/
+                @active_passive = 1, /*active - 0; passive = 1*/
+                @human_rights = 1 ,
+                @ter_sovereignty = 1 ,
+                @peace = 1 ,
+                @securit = 1 , 
+                @democracy = 0 ,
+                @soc_dev = 0 ,
+                @econ_dev = 0 ,
                 @source = 'https://www.consilium.europa.eu/ueDocs/cms_Data/docs/pressData/en/gena/102315.pdf' 
